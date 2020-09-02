200b3a913b7dc1b61c2a7f40a10c805822e4ca4f
unknown <icaroduartec@gmail.com>
2020-09-02 10:09:14 -0300

    Criação do Post publicação e Tema

--- blogpessoal/src/app/service/postagem.service.ts
+++ blogpessoal/src/app/service/postagem.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Postagem } from '../model/Postagem';
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +16,9 @@ export class PostagemService {
   getAllPostagens() {
     return this.http.get("http://localhost:9000/postagens", this.token)
   }
+
+  postPostagem(postagem: Postagem){
+    return this.http.post("http://localhost:9000/postagens", postagem, this.token)
+  }
+
 }
--- blogpessoal/src/app/service/tema.service.ts
+++ blogpessoal/src/app/service/tema.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Tema } from '../model/Tema';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,9 @@ export class TemaService {
   }
   getByIdTema(id: number){
     return this.http.get(`http://localhost:9000/tema/${id}`, this.token)
+  }
+
+  postTema(tema: Tema){
+    return this.http.post("http://localhost:9000/tema", tema, this.token)
   }
 }
